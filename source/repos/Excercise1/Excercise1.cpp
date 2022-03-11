7caf453d75c5a4cd2e4c58a4733528895fbaefbf
devcre <tigertank707@gmail.com>
2022-03-12 00:10:01 +0900

    Day1_printname

---
+++ source/repos/Excercise1/Excercise1.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright KyoungChan Cho. All Rights Reserved.
+
+#include <iostream>
+
+/**
+ * Prints my name and my friend
+ * @return Exit status
+*/
+int main()
+{
+    std::cout << "Kyoung Chan\n";
+    std::cout << "Ha Eun\n";
+}
+
