34d8deb9233314e3547661645e8b44b17040660a
devcre <tigertank707@gmail.com>
2022-03-15 17:57:32 +0900

    Day3_DataTypesVariablesConstants

---
+++ source/repos/Day3/Exercise4.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright 2022. KyoungChan Cho. All Rights Reserved.
+
+#include <iostream>
+
+/**
+ * Calculating with integers
+ * @return Exit status
+*/
+
+int main()
+{
+    int FirstAltitude;
+    int SecondAltitude;
+
+    // Prompt for and get the altitude for a location as an int
+    std::cout << "Enter the first altitude: ";
+    std::cin >> FirstAltitude;
+
+    // Prompt for and get the altitude for a second location as an int
+    std::cout << "Enter the second altitude: ";
+    std::cin >> SecondAltitude;
+
+    // Calculate and display the altitude change you'd experience moving from the first point to the second point
+    std::cout << "Altitude change: " << SecondAltitude - FirstAltitude << "\n";
+}
+
---
+++ source/repos/Day3/IntegerDataTypes.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright 2022 KyoungChan Cho. All Rights Reserved.
+
+#include <iostream>
+
+/**
+ * Integer data types code
+ * @return Exit status
+*/
+int main()
+{
+    const int SecondsPerMinute{ 60 }; // <- 가시성과 유지보수성이 좋다
+    int TotalSecondsPlayed{ 100 };
+
+    // calculate minutes and seconds played
+    int MinutesPlayed = TotalSecondsPlayed / SecondsPerMinute;
+    int SecondsPlayed = TotalSecondsPlayed % SecondsPerMinute;
+
+    // print results
+    std::cout << "Minutes Played: " << MinutesPlayed << "\n";
+    std::cout << "Seconds Played: " << SecondsPlayed << "\n";
+}
+
