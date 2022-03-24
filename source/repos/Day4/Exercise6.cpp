27263cddb429f42090671348eb6384b17e00aa1c
devcre <tigertank707@gmail.com>
2022-03-24 23:53:53 +0900

    add Day4_Exercise5,6

---
+++ source/repos/Day4/Exercise6.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright 2022. KyoungChan Cho. All Rights Reserved.
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <cmath>
+
+/**
+ * Trigonometry with Floats
+ * @return Exit status
+*/
+
+int main()
+{
+    float ValueSin;
+    float ValueCos;
+
+    std::cout << "Enter the degree of sin: ";
+    std::cin >> ValueSin;
+
+    std::cout << "Enter the degree of cos: ";
+    std::cin >> ValueCos;
+
+    std::cout << "Sin " << ValueSin << " degree: " << sinf(ValueSin * M_PI / 180) << "\n";
+    std::cout << "Cos " << ValueCos << " degree: " << cosf(ValueCos * M_PI / 180) << "\n";
+
+}
+
+
